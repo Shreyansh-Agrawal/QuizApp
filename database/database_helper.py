4dc47c8e6101690eae6110c65d53b6e2a16c114f
Shreyansh Agrawal <Shreyansh.Agrawal@watchguard.com>
2023-10-27 11:53:03 +0530

    initialised database: context manager, queries, db helper

---
+++ database/database_helper.py
@@ -0,0 +1,48 @@
+'''
+    Contains methods for establishing database connection
+'''
+
+from typing import List
+from database.database_connection import DatabaseConnection
+
+class DatabaseHelper:
+    ''' A class for database methods
+    '''
+
+    @staticmethod
+    def read_from_database(query: str, data: tuple = None) -> List:
+        '''Reads data from database
+
+        Args:
+            query (str): _description_
+            data (tuple, optional): _description_. Defaults to None.
+
+        Returns:
+            List: _description_
+        '''
+
+        with DatabaseConnection('database\\data.db') as connection:
+            cursor = connection.cursor()
+            if not data:
+                cursor.execute(query)
+            else:
+                cursor.execute(query, data)
+
+        return cursor.fetchall()
+
+    @staticmethod
+    def write_to_database(query: str, data: tuple = None) -> None:
+        ''' CREATE TABLE / Add / Update / Delete data from database
+
+        Args:
+            query (str): _description_
+            data (tuple): _description_
+        '''
+
+        with DatabaseConnection('database\\data.db') as connection:
+            print(connection)
+            cursor = connection.cursor()
+            if not data:
+                cursor.execute(query)
+            else:
+                cursor.execute(query, data)
---
+++ database/queries.py
@@ -0,0 +1,69 @@
+'''
+    Queries for database operations
+'''
+
+class InitializationQueries:
+    '''
+        Contains queries for creation of db tables
+    '''
+
+    CREATE_USERS_TABLE = '''
+        CREATE TABLE IF NOT EXISTS users(
+            user_id TEXT PRIMARY KEY,
+            name TEXT,
+            email TEXT UNIQUE,
+            role TEXT,
+            registration_date TEXT
+        )'''
+
+    CREATE_CREDENTIALS_TABLE= '''
+        CREATE TABLE IF NOT EXISTS credentials(
+            user_id TEXT PRIMARY KEY,
+            username TEXT UNIQUE,
+            password TEXT,
+            isPasswordChanged INTEGER,
+            FOREIGN KEY(user_id) REFERENCES users(user_id) ON DELETE CASCADE
+        )'''
+
+    CREATE_SCORES_TABLE = '''
+        CREATE TABLE IF NOT EXISTS scores(
+            score_id TEXT PRIMARY KEY,
+            user_id TEXT,
+            score INTEGER,
+            timestamp TEXT,
+            FOREIGN KEY(user_id) REFERENCES users(user_id) ON DELETE CASCADE
+        )'''
+
+    CREATE_CATEGORIES_TABLE = '''
+        CREATE TABLE IF NOT EXISTS categories(
+            category_id TEXT PRIMARY KEY,
+            category_name TEXT
+        )'''
+
+    CREATE_QUESTIONS_TABLE = '''
+        CREATE TABLE IF NOT EXISTS questions(
+            question_id TEXT PRIMARY KEY,
+            category_id TEXT,
+            question_text TEXT,
+            question_type TEXT,
+            FOREIGN KEY(category_id) REFERENCES categories(category_id) ON DELETE CASCADE
+        )'''
+
+    CREATE_OPTIONS_TABLE = '''
+        CREATE TABLE IF NOT EXISTS options(
+            option_id TEXT PRIMARY KEY,
+            question_id TEXT,
+            option_text TEXT,
+            isCorrect INTEGER,
+            FOREIGN KEY(question_id) REFERENCES questions(question_id) ON DELETE CASCADE
+        )'''
+
+
+class Queries:
+    '''
+        Contains database queries
+    '''
+
+    INSERT_USER = '''INSERT INTO users VALUES (?, ?, ?, ?, ?)'''
+
+    INSERT_CREDENTIALS = '''INSERT INTO credentials VALUES (?, ?, ?, ?)'''
